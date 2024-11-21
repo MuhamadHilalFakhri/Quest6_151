f26c48a971374de354f65933f479b954d2c9eb1a
hilal555 <muhamadhilal04@gmail.com>
2024-11-21 19:38:07 +0700

    Menambahkan outlinedtextfield

--- app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/widget/DynamicSelectedField.kt
@@ -1,6 +1,15 @@
 package com.example.navigationcompose.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import java.lang.reflect.Modifier
+
+@Composable
 
 fun DynamicSelectTextField(
     selectedValue: String,
@@ -10,5 +19,22 @@ fun DynamicSelectTextField(
     modifier: Modifier = Modifier
 
 ){
+    var expanded by remember { mutableStateOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded},
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+        readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label)},
+            trailingIcon = {
+
+            }
+        )
+    }
 
 }

0c7d725a2e1937d4d70a454d05fba885156aca20
hilal555 <muhamadhilal04@gmail.com>
2024-11-22 13:30:54 +0700

    modifikasi bagian TampilData.kt

--- app/src/main/java/com/example/navigationcompose/ui/view/screen/TampilData.kt
+++ app/src/main/java/com/example/navigationcompose/ui/view/screen/TampilData.kt
@@ -1,5 +1,6 @@
 package com.example.navigationcompose.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -19,8 +22,10 @@ import com.example.navigationcompose.model.Mahasiswa
 import com.example.navigationcompose.model.RencanaStudi
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,53 +41,90 @@ fun TampilView(
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = colorResource(id = R.color.primary)),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .background(color = colorResource(id = R.color.primary))
     ) {
-        Text(
-            text = "Data Mahasiswa",
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            color = Color.White
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Box(
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .background(
-                    color = Color.White,
-                    shape = RoundedCornerShape(15.dp)
-                )
-                .padding(16.dp)
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Column {
-                Text(text = "Informasi Mahasiswa", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "NIM: " + mahasiswa.nim)
-                Text(text = "Nama: " + mahasiswa.nama)
-                Text(text = "Email: " + mahasiswa.email)
-                Spacer(modifier = Modifier.height(16.dp))
+            Image(
+                painter = painterResource(id = R.drawable.umy),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
 
-                Text(text = "Rencana Studi", fontWeight = FontWeight.Bold)
-                Spacer(modifier = Modifier.height(8.dp))
-                Text(text = "Mata Kuliah: " + krs.namaMK)
-                Text(text = "Kelas: " + krs.kelas)
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
             }
         }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(color = colorResource(id = R.color.primary)),
+            horizontalAlignment = Alignment.CenterHorizontally,
 
-        Spacer(modifier = Modifier.height(16.dp))
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Button(onClick = { onBackButtonClicked() }) {
-                Text(text = "Back")
+            Text(
+                text = "Data Mahasiswa",
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp,
+                color = Color.White
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(
+                        color = Color.White,
+                        shape = RoundedCornerShape(15.dp)
+                    )
+                    .padding(16.dp)
+            ) {
+                Column {
+                    Text(text = "Informasi Mahasiswa", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "NIM: " + mahasiswa.nim)
+                    Text(text = "Nama: " + mahasiswa.nama)
+                    Text(text = "Email: " + mahasiswa.email)
+                    Spacer(modifier = Modifier.height(16.dp))
+
+                    Text(text = "Rencana Studi", fontWeight = FontWeight.Bold)
+                    Spacer(modifier = Modifier.height(8.dp))
+                    Text(text = "Mata Kuliah: " + krs.namaMK)
+                    Text(text = "Kelas: " + krs.kelas)
+                }
             }
-            Button(onClick = { onNextButtonClicked() }) {
-                Text(text = "Next")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                Button(onClick = { onBackButtonClicked() }) {
+                    Text(text = "Back")
+                }
+                Button(onClick = { onNextButtonClicked() }) {
+                    Text(text = "Next")
+                }
             }
         }
     }
